d47b9e52bab5707cfb48ec75ab2686ef0805df74
Jeremi Miller <miller.jeremi@gmail.com>
2020-02-22 15:34:53 +0000

    skeleton for the code

---
+++ simulaiton.py
@@ -0,0 +1,28 @@
+# Put imports here
+
+
+# Actual code to run here
+
+
+# Classes here
+
+class Hive(self):
+	"""
+	holds information on indivudal cells, accesses cells and performs operations on cells and cell walls
+	"""
+
+class Cell(self):
+	"""
+	holds information about own walls (height) and base (built, not built)
+	"""
+
+class Bee(self):
+	"""
+	defines bee's behaviour. holds information on position, orientation (?), flying/not flying, weight of carried pulp...
+	"""
+
+class Viewer(self):
+	"""
+	defines how the GUI and the display looks like 
+	"""
+
